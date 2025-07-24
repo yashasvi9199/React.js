222ccea03dab31b901f37e5e55c2f48c8af225ce
Yash Haldiya <yashasvi9199@gmail.com>
2025-07-24 16:02:23 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () =>  {
+return (
+    <nav className="navbar">
+        <div className="links">
+            <a href="/">Home</a> &nbsp;
+            <a href="/create">New Blog</a>
+        </div>
+    </nav>
+
+)
+    };
+
+export default Navbar
