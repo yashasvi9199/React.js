f807984930c566ab39b3d93474a692dc051f786f
Yash Haldiya <yashasvi9199@gmail.com>
2025-07-24 19:49:54 +0530

    Create CSS Style with External component

---
+++ src/components/CssStyle-external.jsx
@@ -0,0 +1,14 @@
+import './CssStyle.css'
+
+const Css = () => {
+
+    return(
+        <div  className="container">
+            <div className="tablet">One</div>
+            <div className="tablet">Two</div>
+            <div className="tablet">Three</div>
+        </div>
+    )
+}
+
+export default Css
