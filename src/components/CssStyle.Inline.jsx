7200a62b9fc15c13a66bfe4da0dfa135daacb5aa
Yash Haldiya <yashasvi9199@gmail.com>
2025-07-24 19:50:11 +0530

    Create CSS Style with inline CSS

---
+++ src/components/CssStyle.Inline.jsx
@@ -0,0 +1,13 @@
+const Css2 = () => {
+
+    return (
+        <div style = { {color: "black", backgroundColor: 'white', display: 'flex'} }>
+            <div>One</div>
+            <div>Two</div>
+            <div>Three</div>
+        </div>
+    )
+
+}
+
+export default Css2;
