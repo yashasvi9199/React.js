9d86fb37b3667e17473923560bbbc85161d0dd6c
Yash Haldiya <yashasvi9199@gmail.com>
2025-07-26 14:15:54 +0530

    Add multiple useEffect examples and name state
    Introduced a new 'name' state and added three useEffect hooks to demonstrate different dependency behaviors. Added a button to change the name state and updated console logs for clarity.

--- src/components/Hook - useEffect.jsx
+++ src/components/Hook - useEffect.jsx
@@ -4,14 +4,28 @@ import { useState, useEffect } from "react";
 
 const ChangeVar = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Yashasvi");
 
+  //! will render at beginning and every change in page
   useEffect(() => {
-    console.log("useEffect function ran");
+    console.log("useEffect function ran without dependency");
   });
+
+  //! will only render at the initial load
+  useEffect(() => {
+    console.log("useEffect function ran with empty dependency");
+  }, [])
+
+  //! will render at beginning and every change in state: name
+  useEffect(() => {
+    console.log("useEffect function ran with dependency");
+    console.log("Name = " + name);
+  }, [name])
 
   return (
     <div>
-        <button onClick={() => setCount((count) => count + 1)}>   count is {count}  </button>
+        <button onClick={() => setCount((count) => count + 1)}>   count is {count}  </button> <br />
+        <button onClick={() => setName("Yash") } > Change </button>
     </div>
   );
 };
