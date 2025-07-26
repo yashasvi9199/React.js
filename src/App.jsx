5ab00508313658160bb7fabb3acb8a147a0b86fc
Yash Haldiya <yashasvi9199@gmail.com>
2025-07-27 00:43:08 +0530

    Add React Router and Route component
    Integrated React Router into App.jsx and added a new RouteComp component. The router now handles navigation to '/router' and displays the RouteComp component, with links for navigation. This sets up basic routing functionality in the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+// Importing React Libraries
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// Importing CSS
 import './App.css';
 
+// Importing Components
 import Navbar from "./components/Navbar";
 import DynamicValue from "./components/DynamicValues";
 import Css from "./components/CssStyle-external";
@@ -9,6 +14,8 @@ import ChangeVariable from "./components/Hook - useState";
 import ChangeVar from "./components/Hook - useEffect";
 import MapCycle from "./components/Map";
 import Parent from "./components/PropsParent";
+import Fetch from "./components/Fetch";
+import RouteComp from "./components/Route";
 
 function App() {
   return (
@@ -62,7 +69,26 @@ function App() {
             <td><Parent /></td>
           </tr>
 
+          <tr>
+            <th>Fetch</th>
+            <td><Fetch /></td>
+          </tr>
 
+          <tr>
+            <th>Router</th>
+            <td>
+              <Router>
+                {/* ! The reason for adding 'exact' keyword is that it should match exact case. If there is a path like /router/create, then without 'exact' keyword, it will also match /router/create and redirect it to /router. As it contains /router in its path */}
+                  <Routes>
+                    <Route exact path= '/router' element={<RouteComp />} />   
+                  </Routes>
+                  <Link to="/router">Router Link</Link> <br />
+                  <Link to="/">Home</Link>
+
+              </Router>
+              
+            </td>
+          </tr>
 
         </tbody>
       </table>
---
+++ src/components/Route.jsx
@@ -0,0 +1,9 @@
+const RouteComp = () => {
+    return(
+        <div>
+            <h2>This is the Route Component</h2>
+        </div>
+    )
+}
+
+export default RouteComp
